7443a27280464137d0c951be53a4093e9710c99f
Safaena123 <safaenabet7@gmail.com>
2020-11-04 12:04:27 +0100

    Capture des flux audio et video

---
+++ js/getUserMedia.js
@@ -0,0 +1,18 @@
+navigator.getUserMedia = navigator.getUserMedia
+|| navigator.webkitGetUserMedia
+|| navigator.mozGetUserMedia;
+var constraints = {audio: false, video: true};
+var video = document.querySelector("video");
+function successCallback(stream) {
+    window.stream = stream;
+    if (window.URL) {
+        video.src = window.URL.createObjectURL(stream);
+    } else {
+        video.src = stream;
+    }
+    video.play();
+}
+function errorCallback(error){
+    console.log("navigator.getUserMedia error: ", error);
+}
+navigator.getUserMedia(constraints, successCallback, errorCallback);
